f9f813136b683d01040f7f165379cdbb0f4e80c3
Carolina Diazgranados <linacello@gmail.com>
2022-11-02 19:00:25 -0400

    tests passed for isPangramic

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  */
@@ -25,7 +27,7 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length - 1];
     }
 
     /**
@@ -33,7 +35,7 @@ public class StringArrayUtils {
      * @return second to last element in specified array
      */ // TODO
     public static String getSecondToLastElement(String[] array) {
-        return null;
+        return array[array.length - 2];
     }
 
     /**
@@ -42,6 +44,12 @@ public class StringArrayUtils {
      * @return true if the array contains the specified `value`
      */ // TODO
     public static boolean contains(String[] array, String value) {
+
+        for (String s : array) {
+            if (value.equals(s)) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -50,7 +58,14 @@ public class StringArrayUtils {
      * @return an array with identical contents in reverse order
      */ // TODO
     public static String[] reverse(String[] array) {
-        return null;
+        String[] array2 = Arrays.copyOf(array, array.length);
+
+        for (int i = 0; i < array.length; i++) {
+            array2[i] = array[(array.length - i) - 1];
+        }
+
+        System.out.println(array2);
+        return array2;
     }
 
     /**
@@ -58,6 +73,11 @@ public class StringArrayUtils {
      * @return true if the order of the array is the same backwards and forwards
      */ // TODO
     public static boolean isPalindromic(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(array[(array.length - i) - 1])) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -66,7 +86,62 @@ public class StringArrayUtils {
      * @return true if each letter in the alphabet has been used in the array
      */ // TODO
     public static boolean isPangramic(String[] array) {
-        return false;
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+        }
+
+        String joinedArray = sb.toString();
+        joinedArray = joinedArray.toLowerCase();
+        joinedArray.replaceAll(" ", "");
+
+
+
+        for (int i = 0; i < alpha.length(); i++) {
+            if (!joinedArray.contains(String.valueOf(alpha.charAt(i)))) {
+                return false;
+            }
+        }
+
+        return true;
+
+//        String alpha = "abcdefghijklmnopqrstuvwxyz";
+//        String[] sa = alpha.split("");
+//
+//
+//        for (String c : sa) {
+//            for (String f : array) {
+//                if (!f.contains(c)) {
+//                    return false;
+//                }
+//            }
+//
+//        }
+//
+//        return true;
+
+
+//        StringBuffer sb = new StringBuffer();
+//        for(int i = 0; i < array.length; i++) {
+//            sb.append(array[i]);
+//        }
+//        String str = sb.toString();
+//        StringBuilder builderAlpha = new StringBuilder(alpha);
+//
+//        for (int i = 0; i < str.length(); i++) {
+//            for (int j = 0; j < str.length(); j ++) {
+//                if (str.charAt(i) == builderAlpha.charAt(j)) {
+//                    builderAlpha = builderAlpha.deleteCharAt(j);
+//                }
+//            }
+//        }
+//
+//       if (alpha.length() == 0) {
+//           return true;
+//       }
+//        return false;
     }
 
     /**
